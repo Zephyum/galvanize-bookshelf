e52155a51b18896cbc213bc2345b0f04a68aabb6
James Dowe <vgkid1@gmail.com>
2017-10-03 09:54:36 -0600

    finished

--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,17 @@
 
 module.exports = {
   development: {
- client: 'pg',
- connection: 'postgres://localhost/bookshelf_dev'
- },
+    client: 'pg',
+    connection: 'postgres://localhost/bookshelf_dev'
+  },
 
   test: {
     client: 'pg',
     connection: 'postgres://localhost/bookshelf_test'
   },
 
-  production: {}
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL
+  }
 };
